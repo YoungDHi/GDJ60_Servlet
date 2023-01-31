a58b6fdc2c9f2cc8f444cad0da065e5cfeae360e
yangjinh94259 <yangjinh94259@gmail.com>
2023-01-31 17:49:10 +0900

    dd

--- src/main/java/com/naver/home/product/ProductService.java
+++ src/main/java/com/naver/home/product/ProductService.java
@@ -1,6 +1,43 @@
 package com.naver.home.product;
 
+import java.util.List;
+
 public class ProductService {
+	
+	//결합도가 높다(강하다)
+	
+	private ProductDAO productDAO = new ProductDAO();
+	
+	{
+		this.productDAO = new ProductDAO();
+	}
+	
+	public ProductService() {
+		
+		this.productDAO = new ProductDAO();
+	}
+	
+	//결합도가 낮다(약하다)
+	public void setProductDAO(ProductDAO productDAO) {
+		this.productDAO=productDAO;
+		
+	}
+	
+	
+	public int setAddProduct(ProductDTO productDTO, List<ProductOptionDTO> ar) throws Exception {
+		//product , option
+		Long productnum = productDAO.getProductNum();
+		productDTO.setProduct_num(productnum);
+		int result = productDAO.setAddProduct(productDTO);
+		
+		for(ProductOptionDTO productOptionDTO:ar) {
+			productOptionDTO.setPrduct_num(productnum);
+			result = productDAO.setAddOption(productOptionDTO);
+		}
+		
+		return result;
+		
+	}
 
 	public static void main(String[] args) {
 		ProductDAO productDAO = new ProductDAO();
